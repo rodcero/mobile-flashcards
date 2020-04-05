ec5177b55f17837cd0d702252a414aaad4a7eaa7
ernie <2838152+rodcero@users.noreply.github.com>
2020-04-05 12:48:22 -0700

    feature: set header navigation based on current tab

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -9,6 +9,7 @@ const BottomTab = createBottomTabNavigator();
 const INITIAL_ROUTE_NAME = 'Home';
 
 export default function BottomTabNavigator({ navigation, route }) {
+  navigation.setOptions({ headerTitle: getHeaderTitle(route) });
   return (
     <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
       <BottomTab.Screen
@@ -35,4 +36,16 @@ export default function BottomTabNavigator({ navigation, route }) {
       />
     </BottomTab.Navigator>
   );
+}
+
+function getHeaderTitle(route) {
+  const routeName =
+    route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
+
+  switch (routeName) {
+    case 'Home':
+      return 'Study';
+    case 'AddDeck':
+      return 'Add Deck';
+  }
 }
