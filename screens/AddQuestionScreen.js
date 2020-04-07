59d44ca43620271272081079c2395f8c35b0df84
ernie <2838152+rodcero@users.noreply.github.com>
2020-04-06 17:18:01 -0700

    feature: add form to add question screen

--- screens/AddQuestionScreen.js
+++ screens/AddQuestionScreen.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Text } from 'react-native';
+import React from "react";
+import PropTypes from "prop-types";
+import { View } from "react-native";
+
+import { Input, Button } from "../components/StyledControls";
+import { MonoText } from "../components/StyledText";
+import {Container}from '../components/StyledLayout'
+import colors from "../constants/Colors"
 
 function AddQuestion(props) {
   //TODO: form question & answer
   //TODO: sumit button, routes back and adds question
-  return <Text>Add Question</Text>;
+  return (
+    <Container>
+      <MonoText>Question:</MonoText>
+      <Input></Input>
+      <MonoText>Answer:</MonoText>
+      <Input></Input>
+      <Button>
+        <MonoText color={colors.light}>ADD</MonoText>
+      </Button>
+    </Container>
+  );
 }
 
 AddQuestion.propTypes = {};
