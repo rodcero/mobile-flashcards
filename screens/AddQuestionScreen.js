11f184c3c18a2a42fdfda97624f07a766ddf5b8d
ernie <2838152+rodcero@users.noreply.github.com>
2020-04-06 17:20:44 -0700

    feature: add question button route back to deck screen

--- screens/AddQuestionScreen.js
+++ screens/AddQuestionScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { View } from "react-native";
+import { useNavigation } from '@react-navigation/native';
 
 import { Input, Button } from "../components/StyledControls";
 import { MonoText } from "../components/StyledText";
@@ -8,15 +9,20 @@ import {Container}from '../components/StyledLayout'
 import colors from "../constants/Colors"
 
 function AddQuestion(props) {
+  const navigation = useNavigation();
   //TODO: form question & answer
   //TODO: sumit button, routes back and adds question
+  const add = () => {
+    navigation.goBack()
+  }
+
   return (
     <Container>
       <MonoText>Question:</MonoText>
       <Input></Input>
       <MonoText>Answer:</MonoText>
       <Input></Input>
-      <Button>
+      <Button onPress={add}>
         <MonoText color={colors.light}>ADD</MonoText>
       </Button>
     </Container>
