02a6947df80408de03294c05116e0221274de86a
ernie <2838152+rodcero@users.noreply.github.com>
2020-04-06 17:06:19 -0700

    feature: added question/answer flow to quiz screen

--- screens/QuizScreen.js
+++ screens/QuizScreen.js
@@ -1,16 +1,40 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { View } from "react-native";
+import { MonoText } from "../components/StyledText";
+import { Container } from "../components/StyledLayout";
+import { Button } from "../components/StyledControls";
+import colors from "../constants/Colors";
 
 function QuizScreen(props) {
-  return (
-    <div>
-      {/* TODO: Quiz question */}
-      {/* TODO: Show answer button */}
-      {/* TODO: Give user option to make their answer correct or incorrect */}
-      {/* TODO: Display number of questions remaining */}
-      {/* TODO: when last question is answered display score */}
+  const [answered, setAnswered] = useState(false);
+
+  {/* TODO: when last question is answered display score */}
       {/* TODO: score screen gives option to 'restart quiz' over or go back to the deckScreen 'Back to Deck' */}
-    </div>
+
+  return (
+    <Container>
+      <MonoText size={20} style={{marginBottom:20}}>1/30 Questions</MonoText>
+      {answered ? (
+        <View>
+          <MonoText>Answered</MonoText>
+          <Button>
+            <MonoText color={colors.light}>CORRECT</MonoText>
+          </Button>
+          <Button>
+            <MonoText color={colors.light}>INCORRECT</MonoText>
+          </Button>
+        </View>
+      ) : (
+        <View>
+          <MonoText>What are props used for?</MonoText>
+          <Button onPress={() => setAnswered(true)}>
+            <MonoText color={colors.light}>SHOW ANSWER</MonoText>
+          </Button>
+        </View>
+      )}
+      
+    </Container>
   );
 }
 
