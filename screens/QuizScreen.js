e4b0c9bed5f12427e8bf9d50a114266e55879ea4
ernie <2838152+rodcero@users.noreply.github.com>
2020-04-08 15:52:31 -0700

    feature: added transition animations betwen cards in quiz

--- screens/QuizScreen.js
+++ screens/QuizScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, Animated } from 'react-native';
 import { MonoText } from '../components/StyledText';
 import { Container } from '../components/StyledLayout';
 import { Button } from '../components/StyledControls';
@@ -26,6 +26,41 @@ function QuizScreen({ navigation, route }) {
   const [answered, setAnswered] = useState(false);
   const [questionIndex, setQuestionIndex] = useState(0);
   const [score, setScore] = useState(0);
+  const [fadeAnim] = useState(new Animated.Value(0));
+  const [slide] = useState(new Animated.Value(0));
+
+  const startFadeAnim = () => {
+    fadeAnim.setValue(0);
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 2000,
+    }).start();
+  };
+
+  const startSlideAnim = () => {
+    Animated.sequence([
+      Animated.timing(slide, {
+        toValue: 10,
+        duration: 200,
+      }),
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 1,
+      }),
+      Animated.timing(slide, {
+        toValue: 0,
+        duration: 1,
+      }),
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 500,
+      }),
+    ]).start();
+  };
+
+  React.useEffect(() => {
+    fadeAnim.setValue(1);
+  }, []);
 
   const onComplete = (correct) => {
     if (correct) setScore(score + 1);
@@ -41,44 +76,78 @@ function QuizScreen({ navigation, route }) {
 
   if (total === questionIndex) {
     clearLocalNotification().then(setLocalNotification);
+    fadeAnim.setValue(0);
+    startFadeAnim();
     return (
       <Container>
-        <MonoText>
-          Score: {score}/{total}
-        </MonoText>
-        <Button onPress={restart}>
-          <MonoText color={colors.light}>Restart</MonoText>
-        </Button>
-        <Button onPress={() => navigation.goBack()}>
-          <MonoText color={colors.light}>Go Back</MonoText>
-        </Button>
+        <Animated.View style={{ opacity: fadeAnim }}>
+          <MonoText>
+            Score: {score}/{total}
+          </MonoText>
+          <Button onPress={restart}>
+            <MonoText color={colors.light}>Restart</MonoText>
+          </Button>
+          <Button onPress={() => navigation.goBack()}>
+            <MonoText color={colors.light}>Go Back</MonoText>
+          </Button>
+        </Animated.View>
       </Container>
     );
   }
 
   return (
     <Container>
-      <MonoText size={20} style={{ marginBottom: 20 }}>
-        {`${questionIndex + 1}/${total}`} Questions
-      </MonoText>
-      {answered ? (
-        <View>
-          <MonoText>{deck.questions[questionIndex].answer}</MonoText>
-          <Button onPress={() => onComplete(true)}>
-            <MonoText color={colors.light}>CORRECT</MonoText>
-          </Button>
-          <Button onPress={() => onComplete(false)}>
-            <MonoText color={colors.light}>INCORRECT</MonoText>
-          </Button>
-        </View>
-      ) : (
-        <View>
-          <MonoText>{deck.questions[questionIndex].question}</MonoText>
-          <Button onPress={() => setAnswered(true)}>
-            <MonoText color={colors.light}>SHOW ANSWER</MonoText>
-          </Button>
-        </View>
-      )}
+      <Animated.View
+        style={{
+          transform: [
+            {
+              translateX: slide.interpolate({
+                inputRange: [0, 10],
+                outputRange: [0, 400],
+              }),
+            },
+          ],
+        }}
+      >
+        <MonoText size={20} style={{ marginBottom: 20 }}>
+          {`${questionIndex + 1}/${total}`} Questions
+        </MonoText>
+        <Animated.View style={{ opacity: fadeAnim }}>
+          {answered ? (
+            <View>
+              <MonoText>{deck.questions[questionIndex].answer}</MonoText>
+              <Button
+                onPress={() => {
+                  onComplete(true);
+                  startSlideAnim();
+                }}
+              >
+                <MonoText color={colors.light}>CORRECT</MonoText>
+              </Button>
+              <Button
+                onPress={() => {
+                  onComplete(false);
+                  startSlideAnim();
+                }}
+              >
+                <MonoText color={colors.light}>INCORRECT</MonoText>
+              </Button>
+            </View>
+          ) : (
+            <View>
+              <MonoText>{deck.questions[questionIndex].question}</MonoText>
+              <Button
+                onPress={() => {
+                  setAnswered(true);
+                  startFadeAnim();
+                }}
+              >
+                <MonoText color={colors.light}>SHOW ANSWER</MonoText>
+              </Button>
+            </View>
+          )}
+        </Animated.View>
+      </Animated.View>
     </Container>
   );
 }
